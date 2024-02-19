d828b7c7abc0d989c1c898f30286086fab833d39
Denis Nikonov <nikonovdenis1991@yandex.ru>
2024-02-19 22:28:38 +0300

    выполнено домашнее задание

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
-//1. проверка длины строки
+//1. проверка длины строки.
 const checkLength = (string, maxLength) => (string.length <= maxLength);
 checkLength ('', 4);
 
 
-//2. проверка палиндрома
+//2. проверка палиндрома.
 const checkPalindrome = (string) => {
   let newString = '';
   string = string.replaceAll(' ','').toLowerCase();
@@ -11,6 +11,20 @@ const checkPalindrome = (string) => {
     newString += string[i];
   }
   return (string === newString);
-}
+};
 checkPalindrome ('');
+
+//3. дополнительное задание. найти цифры в строке.
+const findNumbers = (string) => {
+  let number = '';
+  string = string.toString().replaceAll(' ', '');
+  for (let i = 0; i < string.length; i++) {
+    if((Number(string[i]))) {
+      number += string[i].trim();
+    }
+  }
+  return parseInt(number, 10);
+};
+findNumbers (' a g e n t.00 7 ');
+console.log(findNumbers(' a g e n t.00 7 '))
 
