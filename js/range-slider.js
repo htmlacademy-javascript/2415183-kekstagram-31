f20963425d6d031e2fa35718b98ec966c3b8dbd3
Denis Nikonov <nikonovdenis1991@yandex.ru>
2024-04-06 00:51:26 +0300

    заведены переменные фильтров

---
+++ js/range-slider.js
@@ -0,0 +1,14 @@
+const sliderContainer = document.querySelector('.img-upload__effect-level');
+const sliderElement = document.querySelector('.effect-level__slider');
+const inputValue = document.querySelector('.effect-level__value');
+const items = document.querySelectorAll('.effects__item');
+const image = document.querySelector('.img-upload__preview');
+const STEP = 0.1;
+const MARVIN_STEP = 1;
+const MIN = 0;
+const HEAT_MIN = 1;
+const MAX = 1;
+const MARVIN_MAX = 100;
+const PHOBOS_MAX = 3;
+const HEAT_MAX = 3;
+
